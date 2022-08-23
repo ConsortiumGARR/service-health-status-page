b0147b07ff7866ebf586bf96f90635a7638d3e6f
Francesco Lombardo <francesco.lombardo@garr.it>
2022-08-23 17:25:57 +0200

    add O~S Object Storage as data source

--- status_page_app/utils.py
+++ status_page_app/utils.py
@@ -1,11 +1,48 @@
 from datetime import datetime, timezone
-import json, os
+import json
+import os
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+from status_page_app.error_handler import *
+
+
+def request_data():
+    try:
+        session = requests.Session()
+        retry = Retry(connect=6, backoff_factor=0.5)
+        adapter = HTTPAdapter(max_retries=retry)
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
+        url = 'https://swift.cloud.garr.it/swift/v1/neanias-status-page/status-page-data.json'
+        resp = session.get(url)
+    except (requests.ConnectionError, requests.Timeout, requests.TooManyRedirects, requests.URLRequired) as e:
+        raise ServerError('Object Storage connection error: ' + str(e))
+    try:
+        resp.raise_for_status()
+    except requests.HTTPError as e:
+        if e.response.status_code == 400:
+            raise BadRequest(description=e.response.text)
+        elif e.response.status_code == 401:
+            raise Unauthorized(description=e.response.text)
+        elif e.response.status_code == 404:
+            raise ResourceNotFound(description=e.response.text)
+        else:
+            raise ServerError(description=e.response.text)
+    try:
+        ctype = resp.headers['content-type']
+    except KeyError:
+        # success but no content
+        return None, resp.headers
+    if 'application/json' in ctype:
+
+        return resp.json(), resp.headers
+    else:
+        return resp.text, resp.headers
+
 
 def init_services_list(base_list):
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, "data", "svc.json")
-    base_list = json.load(open(json_url))
-   
+    base_list, resp_code = request_data()
     for s in base_list:
         base_list[s]['status'] = None
     return base_list
